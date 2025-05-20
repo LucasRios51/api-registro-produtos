87560d5989ac1ca773967e3c96ffe8f0bdd08ec4
LucasRios <lucasriosbarcelos1996@gmail.com>
2025-05-20 15:50:20 -0300

    database adicionado

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,31 +4,6 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_ola_mundo():
-    response = client.get("/")
-    assert response.status_code == 200
-
-
-def test_ola_mundo_json():
-    response = client.get("/")
-    assert response.json() == {"Olá": "Mundo"}
-
-
 def test_listar_produtos_status_code():
     response = client.get("/produtos")
     assert response.status_code == 200
-
-
-def test_tamnho_da_lista_de_produtos():
-    response = client.get("/produtos")
-    assert len(response.json()) == 3
-
-
-def test_pega_um_produto():
-    response = client.get("/produtos/1")
-    assert response.json() == {
-        "id": 1,
-        "nome": "Smartphone",
-        "descricao": "Um telefone que é inteligente",
-        "preco": 15000.00,
-    }
